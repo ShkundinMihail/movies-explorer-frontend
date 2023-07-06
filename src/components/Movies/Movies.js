1e0cf6eca71d2daa983035b2906f5dd44c0bf500
John Doe <johndoe@example.com>
2023-07-06 20:51:12 +0300

    запрос к серверу фильмов только при первом поиске

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -4,7 +4,7 @@ import SearchForm from '../SearchForm/SearchForm.js';
 import Preloader from '../Preloader/Preloader.js';
 import MoviesCardList from '../MoviesCardList/MoviesCardList';
 import { getMovies } from '../../utils/MoviesApi';
-import { CYRILLIC_REGEX, SCREEN_WIDTH_1280,SCREEN_WIDTH_768, DURATION_SHORT_FILM } from '../../utils/constants';
+import { CYRILLIC_REGEX, SCREEN_WIDTH_1280, SCREEN_WIDTH_768, DURATION_SHORT_FILM, ALL_MOVIES_FROM_LOCAL_STORAGE } from '../../utils/constants';
 
 const Movies = ({ windowWidth, shortFilmsCheckbox, setShortFilmsCheckbox, numberFilms, setNumberFilms, addFilmToUser, setPreloader, preloader, savedFilms, setSavedFilms, deleteUsersFilm, searchText, setSearchText, infoText, setInfoText, searchResult, setSearchResult }) => {
 
@@ -14,8 +14,8 @@ const Movies = ({ windowWidth, shortFilmsCheckbox, setShortFilmsCheckbox, number
         } else if (windowWidth > SCREEN_WIDTH_768 && windowWidth < SCREEN_WIDTH_1280) {
             setNumberFilms(numberFilms + 2)
         } else {
-            setNumberFilms(numberFilms + 1)//Если карточки уже были отображены на странице в блоке результатов, клик по чекбоксу «Короткометражки» приводит к повторной фильтрации результата.
-            
+            setNumberFilms(numberFilms + 1)
+
         }
     };
 
@@ -31,33 +31,34 @@ const Movies = ({ windowWidth, shortFilmsCheckbox, setShortFilmsCheckbox, number
         setPreloader(true);
         localStorage.setItem('searchText', searchText);
         let data
-        getMovies()
-            .then((res) => {
-                if (CYRILLIC_REGEX.test(String(searchText).toLowerCase())) {
-                    data = res.filter(({ nameRU }) => nameRU.toLowerCase().includes(searchText.toLowerCase()));
-                } else {
-                    data = res.filter(({ nameEN }) => nameEN.toLowerCase().includes(searchText.toLowerCase()));
-                }
-                if (shortFilmsCheckbox) {
-                    data = data.filter(({ duration }) => duration <= DURATION_SHORT_FILM)
-                }
-                if (data.length === 0) {
-                    setSearchResult([]);
+        if (!ALL_MOVIES_FROM_LOCAL_STORAGE) {
+            getMovies()
+                .then((res) => {
+                    localStorage.setItem('allMovies', JSON.stringify(res));
+                })
+                .catch((err) => {
                     localStorage.setItem('movies', []);
-                    setInfoText('Ничего не найдено');
-                } else {
-                    localStorage.setItem('movies', JSON.stringify(data));
-                    setSearchResult(data);
-                };
-            })
-            .catch(() => {
-                localStorage.setItem('movies', []);
-                setSearchResult([]);
-                setInfoText('Во время запроса произошла ошибка. Возможно, проблема с соединением или сервер недоступен. Подождите немного и попробуйте ещё раз');
-            })
-            .finally(() => {
-                setPreloader(false);
-            })
+                    setSearchResult([]);
+                    setInfoText(`Во время запроса произошла ошибка ${err.message}. Возможно, проблема с соединением или сервер недоступен. Подождите немного и попробуйте ещё раз`);
+                })
+        }
+        if (CYRILLIC_REGEX.test(String(searchText).toLowerCase())) {
+            data = JSON.parse(ALL_MOVIES_FROM_LOCAL_STORAGE).filter(({ nameRU }) => nameRU.toLowerCase().includes(searchText.toLowerCase()));
+        } else {
+            data = JSON.parse(ALL_MOVIES_FROM_LOCAL_STORAGE).filter(({ nameEN }) => nameEN.toLowerCase().includes(searchText.toLowerCase()));
+        }
+        if (shortFilmsCheckbox) {
+            data = data.filter(({ duration }) => duration <= DURATION_SHORT_FILM)
+        }
+        if (data.length === 0) {
+            setSearchResult([]);
+            localStorage.setItem('movies', []);
+            setInfoText('Ничего не найдено');
+        } else {
+            localStorage.setItem('movies', JSON.stringify(data));
+            setSearchResult(data);
+        };
+        setPreloader(false);
     };
 
     return (
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -9,6 +9,7 @@ export const FILMS_FROM_LOCAL_STORAGE = localStorage.getItem('movies');
 export const STATUS_SHORT_FILMS_CHECKBOX_FROM_LOCAL_STORAGE = localStorage.getItem('checkboxShortFilms');
 export const SEARCH_TEXT_FROM_LOCAL_STORAGE = localStorage.getItem('searchText');
 export const TOKEN = localStorage.getItem('token');
+export const ALL_MOVIES_FROM_LOCAL_STORAGE = localStorage.getItem('allMovies');
 export const SCREEN_WIDTH_1280 = 1280;
 export const SCREEN_WIDTH_768 = 768;
 export const DURATION_SHORT_FILM = 40;
