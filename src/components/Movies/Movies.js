f9ca78f4d213c9eb375fea859d097a613a3aa1ac
SkhundinMichail <113530482+SkhundinMichail@users.noreply.github.com>
2023-07-11 23:41:50 +0300

    возможно(но не точно) удалось исправить поиск на странице с фильмами

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,110 +1,182 @@
-import React from 'react';
-import './Movies.css';
-import SearchForm from '../SearchForm/SearchForm.js';
-import Preloader from '../Preloader/Preloader.js';
-import MoviesCardList from '../MoviesCardList/MoviesCardList';
-import { getMovies } from '../../utils/MoviesApi';
-import { CYRILLIC_REGEX, SCREEN_WIDTH_1280, SCREEN_WIDTH_768, DURATION_SHORT_FILM, ALL_MOVIES_FROM_LOCAL_STORAGE, STATUS_SHORT_FILMS_CHECKBOX_FROM_LOCAL_STORAGE } from '../../utils/constants';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React from "react";
+import "./Movies.css";
+import SearchForm from "../SearchForm/SearchForm.js";
+import Preloader from "../Preloader/Preloader.js";
+import MoviesCardList from "../MoviesCardList/MoviesCardList";
+import { getMovies } from "../../utils/MoviesApi";
+import {
+  CYRILLIC_REGEX,
+  SCREEN_WIDTH_1280,
+  SCREEN_WIDTH_768,
+  DURATION_SHORT_FILM,
+} from "../../utils/constants";
 
-const Movies = ({ searchTextFromMovies, setSearchTextFromMovies, windowWidth, addFilmToUser, setPreloader, preloader, savedFilms, setSavedFilms, deleteUsersFilm, infoText, setInfoText, searchResult, setSearchResult }) => {
-    const [shortFilmsCheckbox, setShortFilmsCheckbox] = React.useState(STATUS_SHORT_FILMS_CHECKBOX_FROM_LOCAL_STORAGE === 'true' ? true : false);
-    const [numberFilms, setNumberFilms] = React.useState(0);
+const Movies = ({
+  searchTextFromMovies,
+  setSearchTextFromMovies,
+  windowWidth,
+  addFilmToUser,
+  setPreloader,
+  preloader,
+  savedFilms,
+  setSavedFilms,
+  deleteUsersFilm,
+  infoText,
+  setInfoText,
+  searchResult,
+  setSearchResult,
+}) => {
+  const [shortFilmsCheckbox, setShortFilmsCheckbox] = React.useState(
+    localStorage.getItem("checkboxShortFilms") === "true" ? true : false
+  );
+  const [numberFilms, setNumberFilms] = React.useState(0);
 
-    React.useEffect(() => {
-        setShortFilmsCheckbox(STATUS_SHORT_FILMS_CHECKBOX_FROM_LOCAL_STORAGE === 'true' ? true : false);
+  React.useEffect(() => {
+    if (shortFilmsCheckbox && getShortFilms(searchResult).length === 0) {
+      setInfoText("Ничего не найдено");
+    }
+  }, [shortFilmsCheckbox]);
 
-    }, []);
+  React.useEffect(() => {
+    if (windowWidth >= SCREEN_WIDTH_1280) {
+      setNumberFilms(12);
+    } else if (
+      windowWidth > SCREEN_WIDTH_768 &&
+      windowWidth < SCREEN_WIDTH_1280
+    ) {
+      setNumberFilms(8);
+    } else {
+      setNumberFilms(5);
+    }
+  }, [windowWidth]);
 
-    React.useEffect(() => {
-        localStorage.setItem('checkboxShortFilms', shortFilmsCheckbox);
-    }, [shortFilmsCheckbox]);
+  const handleMoreFilms = () => {
+    if (windowWidth >= SCREEN_WIDTH_1280) {
+      setNumberFilms(numberFilms + 3);
+    } else if (
+      windowWidth > SCREEN_WIDTH_768 &&
+      windowWidth < SCREEN_WIDTH_1280
+    ) {
+      setNumberFilms(numberFilms + 2);
+    } else {
+      setNumberFilms(numberFilms + 1);
+    }
+  };
 
-    React.useEffect(() => {
-        if (windowWidth >= SCREEN_WIDTH_1280) {
-            setNumberFilms(12)
-        } else if (windowWidth > SCREEN_WIDTH_768 && windowWidth < SCREEN_WIDTH_1280) {
-            setNumberFilms(8)
-        } else {
-            setNumberFilms(5)
-        }
-    }, [windowWidth]);
+  const moreFilmsButtonVisible = (films) => {
+    if (
+      films.length === 0 ||
+      (films.length <= 12 && windowWidth >= SCREEN_WIDTH_1280) ||
+      (films.length <= 8 &&
+        windowWidth > SCREEN_WIDTH_768 &&
+        windowWidth < SCREEN_WIDTH_1280) ||
+      (films.length <= 5 && windowWidth < SCREEN_WIDTH_768) ||
+      films.length <= numberFilms
+    ) {
+      return "movies__button-more movies__button-more_hidden";
+    } else {
+      return "movies__button-more";
+    }
+  };
 
-    const handleMoreFilms = () => {
-        if (windowWidth >= SCREEN_WIDTH_1280) {
-            setNumberFilms(numberFilms + 3)
-        } else if (windowWidth > SCREEN_WIDTH_768 && windowWidth < SCREEN_WIDTH_1280) {
-            setNumberFilms(numberFilms + 2)
-        } else {
-            setNumberFilms(numberFilms + 1)
+  const handleFilms = () => {
+    setPreloader(true);
+    localStorage.setItem("searchText", searchTextFromMovies);
+    if (!localStorage.getItem("allMovies")) {
+      getMovies()
+        .then((res) => {
+          localStorage.setItem("allMovies", JSON.stringify(res));
+          movieSearchLogic(res);
+        })
+        .catch((err) => {
+          localStorage.setItem("movies", []);
+          setSearchResult([]);
+          setInfoText(
+            `Во время запроса произошла ошибка ${err.message}. Возможно, проблема с соединением или сервер недоступен. Подождите немного и попробуйте ещё раз`
+          );
+        })
+        .finally(() => {
+          setPreloader(false);
+        });
+    } else {
+      const allMovies = JSON.parse(localStorage.getItem("allMovies"));
+      movieSearchLogic(allMovies);
+      setPreloader(false);
+    }
+  };
+  const movieSearchLogic = (array) => {
+    let data;
+    if (CYRILLIC_REGEX.test(String(searchTextFromMovies).toLowerCase())) {
+      data = array.filter(({ nameRU }) =>
+        nameRU.toLowerCase().includes(searchTextFromMovies.toLowerCase())
+      );
+    } else {
+      data = array.filter(({ nameEN }) =>
+        nameEN.toLowerCase().includes(searchTextFromMovies.toLowerCase())
+      );
+    }
+    if (data.length === 0) {
+      setSearchResult([]);
+      localStorage.setItem("movies", []);
+      setInfoText("Ничего не найдено");
+    } else {
+      localStorage.setItem("movies", JSON.stringify(data));
+      setSearchResult(data);
+    }
+  };
 
-        }
-    };
+  const getShortFilms = (films) => {
+    return films.filter(({ duration }) => duration <= DURATION_SHORT_FILM);
+  };
 
-    const moreFilmsButtonVisible = () => {
-        if (searchResult.length === 0 || (searchResult.length <= 12 && windowWidth >= SCREEN_WIDTH_1280) || (searchResult.length <= 8 && windowWidth > SCREEN_WIDTH_768 && windowWidth < SCREEN_WIDTH_1280) || (searchResult.length <= 5 && windowWidth < SCREEN_WIDTH_768) || (searchResult.length <= numberFilms)) {
-            return 'movies__button-more movies__button-more_hidden'
-        } else {
-            return 'movies__button-more'
-        }
-    };
-
-    const handleFilms = () => {
-        setPreloader(true);
-        localStorage.setItem('searchText', searchTextFromMovies);
-        if (!ALL_MOVIES_FROM_LOCAL_STORAGE) {
-            getMovies()
-                .then((res) => {
-                    localStorage.setItem('allMovies', JSON.stringify(res));
-                    movieSearchLogic(res);
-                })
-                .catch((err) => {
-                    localStorage.setItem('movies', []);
-                    setSearchResult([]);
-                    setInfoText(`Во время запроса произошла ошибка ${err.message}. Возможно, проблема с соединением или сервер недоступен. Подождите немного и попробуйте ещё раз`);
-                })
-                .finally(() => {
-                    setPreloader(false);
-                });
-        } else {
-            const allMovies = JSON.parse(ALL_MOVIES_FROM_LOCAL_STORAGE);
-            movieSearchLogic(allMovies);
-            setPreloader(false);
-        }
-    };
-    const movieSearchLogic = (array) => {
-        let data
-        if (CYRILLIC_REGEX.test(String(searchTextFromMovies).toLowerCase())) {
-            data = array.filter(({ nameRU }) => nameRU.toLowerCase().includes(searchTextFromMovies.toLowerCase()));
-        } else {
-            data = array.filter(({ nameEN }) => nameEN.toLowerCase().includes(searchTextFromMovies.toLowerCase()));
-        }
-        if (shortFilmsCheckbox) {
-            data = data.filter(({ duration }) => duration <= DURATION_SHORT_FILM)
-        }
-        if (data.length === 0) {
-            setSearchResult([]);
-            localStorage.setItem('movies', []);
-            setInfoText('Ничего не найдено');
-        } else {
-            localStorage.setItem('movies', JSON.stringify(data));
-            setSearchResult(data);
-        };
-    };
-
-    return (
-        <div className='movies'>
-            <SearchForm searchText={searchTextFromMovies} setSearchText={setSearchTextFromMovies} handleFilms={handleFilms} shortFilmsCheckbox={shortFilmsCheckbox} setShortFilmsCheckbox={setShortFilmsCheckbox} />
-            {preloader ? <Preloader /> :
-                <>
-                    {(!searchResult || searchResult.length === 0) ? <p className='movies__info-text'>{infoText}</p> :
-                        <>
-                            <MoviesCardList numberFilms={numberFilms} movies={searchResult} addFilmToUser={addFilmToUser} savedFilms={savedFilms} setSavedFilms={setSavedFilms} deleteUsersFilm={deleteUsersFilm} />
-                            <button className={moreFilmsButtonVisible()} onClick={handleMoreFilms}>Ещё</button>
-                        </>}
-
-                </>}
-        </div>
-    );
+  return (
+    <div className="movies">
+      <SearchForm
+        searchText={searchTextFromMovies}
+        setSearchText={setSearchTextFromMovies}
+        handleFilms={handleFilms}
+        shortFilmsCheckbox={shortFilmsCheckbox}
+        setShortFilmsCheckbox={setShortFilmsCheckbox}
+      />
+      {preloader ? (
+        <Preloader />
+      ) : (
+        <>
+          {!searchResult ||
+          searchResult.length === 0 ||
+          (shortFilmsCheckbox && getShortFilms(searchResult).length === 0) ? (
+            <p className="movies__info-text">{infoText}</p>
+          ) : (
+            <>
+              <MoviesCardList
+                numberFilms={numberFilms}
+                movies={
+                  !shortFilmsCheckbox
+                    ? searchResult
+                    : getShortFilms(searchResult)
+                }
+                addFilmToUser={addFilmToUser}
+                savedFilms={savedFilms}
+                setSavedFilms={setSavedFilms}
+                deleteUsersFilm={deleteUsersFilm}
+              />
+              <button
+                className={
+                  !shortFilmsCheckbox
+                    ? moreFilmsButtonVisible(searchResult)
+                    : moreFilmsButtonVisible(getShortFilms(searchResult))
+                }
+                onClick={handleMoreFilms}
+              >
+                Ещё
+              </button>
+            </>
+          )}
+        </>
+      )}
+    </div>
+  );
 };
 
 export default Movies;
--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,66 +1,97 @@
 import React from "react";
-import './SearchForm.css';
-import searchIcon from '../../images/iconSearch.svg';
-import searchFormButton from '../../images/findsearch.svg';
+import "./SearchForm.css";
+import searchIcon from "../../images/iconSearch.svg";
+import searchFormButton from "../../images/findsearch.svg";
 
-const SearchForm = ({ setSearchText, searchText, handleFilms, shortFilmsCheckbox, setShortFilmsCheckbox }) => {
-    const [blurInput, setBlurInput] = React.useState(false);
-    const [focusInput, setFocusInput] = React.useState(false);
-   
-    const handleShortChange = () => {
-        setShortFilmsCheckbox(!shortFilmsCheckbox);
-    };
+const SearchForm = ({
+  setSearchText,
+  searchText,
+  handleFilms,
+  shortFilmsCheckbox,
+  setShortFilmsCheckbox,
+}) => {
+  const [blurInput, setBlurInput] = React.useState(false);
+  const [focusInput, setFocusInput] = React.useState(false);
 
-    const changeInput = (e) => {
-        setSearchText(e.target.value);
-    };
+  const handleShortChange = () => {
+    setShortFilmsCheckbox(shortFilmsCheckbox ? false : true);
+    localStorage.setItem("checkboxShortFilms", !shortFilmsCheckbox); //не понял где накосячил, в общем метод тыка
+  };
 
-    return (
-        <div className="search-form">
-            <form className="search-form__form">
-                {focusInput && <>
-                    <div className={blurInput ? "search-form__form-input-blur" : "search-form__form-input-focus"}></div>
-                    <div className={blurInput ? 'search-form__form-input-blur search-form__form-input-blur_top' : "search-form__form-input-focus search-form__form-input-focus_top"}></div>
-                </>}
-                <img className="search-form__image" src={searchIcon} alt="iconSearch" />
-                <input
-                    onFocus={() => { setBlurInput(false); setFocusInput(true) }}
-                    onBlur={() => { setBlurInput(true) }}
-                    onChange={changeInput}
-                    maxLength='200'
-                    className="search-form__input"
-                    type="text"
-                    placeholder="Фильм"
-                    required
-                    value={searchText ? searchText : ''}
-                />
-                <button
-                    className="search-form__button"
-                    type="submit"
-                    onClick={(e) => {
-                        e.preventDefault();
-                        handleFilms();
-                    }}
-                    disabled={searchText === ''}>
-                    <img className="search-form__button-image" src={searchFormButton} alt="findsearch" />
-                </button>
-            </form>
-            <div className="search-form__dividing-line"></div>
-            <div className="search-form__shorts-container">
-                <label className="search-form__shorts-button">
-                    <input
-                        type="checkbox"
-                        className="search-form__shorts-checkbox"
-                        value={shortFilmsCheckbox}
-                        checked={shortFilmsCheckbox}
-                        onChange={handleShortChange}
-                    />
-                    <span className="search-form__shorts-slider" />
-                </label>
-                <p className="search-form__shorts-text">Короткометражки</p>
-            </div>
-        </div>
-    );
+  const changeInput = (e) => {
+    setSearchText(e.target.value);
+  };
+
+  return (
+    <div className="search-form">
+      <form className="search-form__form">
+        {focusInput && (
+          <>
+            <div
+              className={
+                blurInput
+                  ? "search-form__form-input-blur"
+                  : "search-form__form-input-focus"
+              }
+            ></div>
+            <div
+              className={
+                blurInput
+                  ? "search-form__form-input-blur search-form__form-input-blur_top"
+                  : "search-form__form-input-focus search-form__form-input-focus_top"
+              }
+            ></div>
+          </>
+        )}
+        <img className="search-form__image" src={searchIcon} alt="iconSearch" />
+        <input
+          onFocus={() => {
+            setBlurInput(false);
+            setFocusInput(true);
+          }}
+          onBlur={() => {
+            setBlurInput(true);
+          }}
+          onChange={changeInput}
+          maxLength="200"
+          className="search-form__input"
+          type="text"
+          placeholder="Фильм"
+          required
+          value={searchText ? searchText : ""}
+        />
+        <button
+          className="search-form__button"
+          type="submit"
+          onClick={(e) => {
+            e.preventDefault();
+            handleFilms();
+          }}
+          disabled={searchText === ""}
+        >
+          <img
+            className="search-form__button-image"
+            src={searchFormButton}
+            alt="findsearch"
+          />
+        </button>
+      </form>
+      <div className="search-form__dividing-line"></div>
+      <div className="search-form__shorts-container">
+        <label className="search-form__shorts-button">
+          <input
+            type="checkbox"
+            className="search-form__shorts-checkbox"
+            value={shortFilmsCheckbox}
+            checked={shortFilmsCheckbox}
+            onChange={handleShortChange}
+          />
+          <span className="search-form__shorts-slider" />
+        </label>
+        <p className="search-form__shorts-text">Короткометражки</p>
+      </div>
+    </div>
+  );
 };
 
 export default SearchForm;
